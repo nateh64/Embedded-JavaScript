0372d9700261f3526a6b4cb2b70b6b7c2bfee09d
nateh64 <7nhuzenis@stpatrick.org>
2017-03-23 13:53:56 -0500

    Add files via upload

---
+++ Embedded-JavaScript-master/JavaScriptQueries-master/click.js
@@ -0,0 +1,14 @@
+//declare samecolor() here
+function samecolor(color){
+  const doc = document;
+  let divs = doc.querySelectorAll("#samecolor div");
+  for(let i = 0;i<h1.length;i++){
+    div[i] = style.backgroundColor ="#"+color;
+    div[i] = style.opacity = Math.random();
+  }
+}
+//declare diffcolor() here
+function diffcolor(){
+  const doc = document;
+
+}
